ca5ed1a02b7f9c05d7e6584fdaf50cd8ffcc31af
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-22 18:59:57 -0300

    feat: initially config

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/vtx-payout-microservice-user/config"
+)
+
+func main() {
+
+	config, err := config.Load("config/config.yaml")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	db, err := sql.Open("postgres", newPostgres(config.Database))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	err = migrate(db, config.Database.Schema)
+	if err != nil {
+		log.Fatalf("failed to execute migrations: %v", err)
+	}
+
+	router := gin.Default()
+
+	log.Printf("initing service: %s", config.Name)
+	router.Run(fmt.Sprintf(":%d", config.Server.Port))
+
+}
+
+func newPostgres(dbconfig config.Database) string {
+	return "user=" + dbconfig.User +
+		" password=" + dbconfig.Password +
+		" dbname=" + dbconfig.Name +
+		" host=" + dbconfig.Host +
+		" port=" + dbconfig.Port +
+		" sslmode=disable"
+}
+
+func migrate(db *sql.DB, filepath string) error {
+	schema, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(string(schema))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
