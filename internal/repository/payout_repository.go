37c02c1af68a6fd1d58f0aa5446ec46ed95f6b05
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-23 15:51:09 -0300

    feat: payout repository

--- internal/repository/payout_repository.go
+++ internal/repository/payout_repository.go
@@ -10,8 +10,8 @@ import (
 
 type IPayoutRepository interface {
 	CreatePayout(ctx context.Context, payout *models.Payout) error
-	ReadPayout(ctx context.Context, route *string) (*models.Payout, error)
-	DeletePayout(ctx context.Context, uuid *uuid.UUID) error
+	ReadPayout(ctx context.Context, id *uuid.UUID) (*models.Payout, error)
+	DeletePayout(ctx context.Context, id *uuid.UUID) error
 }
 
 type PayoutRepository struct {
@@ -22,4 +22,55 @@ func NewPayoutRepository(conn *sql.DB) *PayoutRepository {
 	return &PayoutRepository{
 		db: conn,
 	}
+}
+
+func (r *PayoutRepository) CreatePayout(ctx context.Context, payout *models.Payout) error {
+
+	sqlQuery := `INSERT INTO payouts (id, payout, route) VALUES ($1, $2, $3)`
+
+	_, err := r.db.Exec(sqlQuery, payout.ID, payout.Payout, payout.Route)
+
+	return err
+
+}
+
+func (r *PayoutRepository) ReadPayout(ctx context.Context, id *uuid.UUID) (*models.Payout, error) {
+
+	sqlQuery := `SELECT id, payout, route FROM payouts WHERE id = $1 LIMIT 1`
+
+	var payout models.Payout
+
+	err := r.db.QueryRow(sqlQuery, *id).Scan(
+		&payout.ID,
+		&payout.Payout,
+		&payout.Route,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &payout, nil
+
+}
+
+func (r *PayoutRepository) DeletePayout(ctx context.Context, id *uuid.UUID) error {
+
+	tx, err := r.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+	_, err = tx.Exec("DELETE FROM payouts WHERE id = $1", *id)
+	return err
+
 }
